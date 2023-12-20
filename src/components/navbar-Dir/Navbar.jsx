884b7dd938f74bd14d7f74d00e0a2caff264008b
Samuvel6826 <samuvel6826@gmail.com>
2023-12-20 17:01:45 +0530

    offset modified for Projects Section

--- src/components/navbar-Dir/Navbar.jsx
+++ src/components/navbar-Dir/Navbar.jsx
@@ -32,7 +32,7 @@ function Navbar() {
         <div className="desktopMenu">
             <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
             <Link activeClass='active' to='about' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">About</Link>
-            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Projects</Link>
+            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-60} duration={500} className="desktopMenuListItem">Projects</Link>
             <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className="desktopMenuListItem">Contact</Link>
         </div>
         <button className="desktopMenuBtn" onClick={()=>{document.getElementById('contact').scrollIntoView({behavior: 'smooth'})}}>
