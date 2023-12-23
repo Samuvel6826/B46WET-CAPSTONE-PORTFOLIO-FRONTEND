d84f1d91ba19a0bc1e8c15aa11ddb62908ae035b
Samuvel6826 <samuvel6826@gmail.com>
2023-12-23 15:44:45 +0530

    Setting mobile

--- src/components/navbar-Dir/Navbar.jsx
+++ src/components/navbar-Dir/Navbar.jsx
@@ -31,10 +31,10 @@ function Navbar() {
         <img src={Logo} alt="Logo" className='logo'/>
         <div className="desktopMenu">
             <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Home</Link>
-            <Link activeClass='active' to='certificate' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Certificate</Link>
+            {/* <Link activeClass='active' to='certificate' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Certificate</Link> */}
             <Link activeClass='active' to='about' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">About</Link>
-            <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Projects</Link>
-            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Contact</Link>
+            {/* <Link activeClass='active' to='projects' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Projects</Link>
+            <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-80} duration={500} className="desktopMenuListItem">Contact</Link> */}
         </div>
         <button className="desktopMenuBtn" onClick={()=>{document.getElementById('contact').scrollIntoView({behavior: 'smooth'})}}>
             <img src={contactImg} alt="Contact Image" className="desktopMenuImg" />Contact Me
